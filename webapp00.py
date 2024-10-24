0c614721eef7a7dc6ac93884aecccd2c528e4894
2303886 <2303886@escolas.anchieta.br>
2024-10-23 21:57:47 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title("MEU 1º WEB APP STREAMLIT")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Larissa quero brigadeiro")
+st.header("Quando tem encomenda?")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Sub Cabeçalho")
